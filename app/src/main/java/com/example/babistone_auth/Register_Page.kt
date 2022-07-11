c3dc1694686100f79514a28aab8b9bd55c79b8d5
Babi Mumba <babimumba243@gmail.com>
2022-07-11 22:43:46 +0200

    add anew configuration

--- app/src/main/java/com/example/babistone_auth/Register_Page.kt
+++ app/src/main/java/com/example/babistone_auth/Register_Page.kt
@@ -5,15 +5,29 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.register_page.*
 
 class Register_Page : AppCompatActivity() {
     lateinit var sharedPreferences: SharedPreferences
+    lateinit var etEmail: EditText
+    lateinit var etConfPass: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+    lateinit var tvRedirectLogin: TextView
+
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_page)
+        /*
         sharedPreferences = this.getSharedPreferences("app_state", Context.MODE_PRIVATE)
         val isAuthentificated = sharedPreferences.getBoolean("is_authentificated", false)
         val nomSharedPreferences = sharedPreferences.getString("nom", "")
@@ -23,6 +37,13 @@ class Register_Page : AppCompatActivity() {
                 startActivity(it)
             }
         }
+         */
+
+        etEmail = findViewById(R.id.mail)
+        etConfPass = findViewById(R.id.confirmer_password)
+        etPass = findViewById(R.id.passeword)
+        btnSignUp = findViewById(R.id.btn_valider)
+
             title = "Creation du Compte"
             compte.setOnClickListener {
                 Intent(this, Login_Page::class.java).also {
@@ -38,10 +59,13 @@ class Register_Page : AppCompatActivity() {
                     confirmer_password.error = "mot de passe different"
                 } else {
 
-                    val editor = sharedPreferences.edit()
+                    /*
+                     val editor = sharedPreferences.edit()
                     editor.putBoolean("is_authentificated", true)
                     editor.putString("nom", mail.text.toString())
                     editor.apply()
+                     */
+
                     var act_home = Intent(this, Home_pageActivity::class.java)
                     startActivity(act_home)
                     finish()
