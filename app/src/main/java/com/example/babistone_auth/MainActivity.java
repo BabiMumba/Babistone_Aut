991c00d145415fd170e64875b05a2f9ea865e96a
Babi Mumba <babimumba243@gmail.com>
2022-07-14 21:38:09 +0200

    add a new commrnt

--- app/src/main/java/com/example/babistone_auth/MainActivity.java
+++ app/src/main/java/com/example/babistone_auth/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.babistone_auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.ramotion.paperonboarding.PaperOnboardingFragment;
+import com.ramotion.paperonboarding.PaperOnboardingPage;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        // new instance is created and data is took from an
+        // array list known as getDataonborading
+        final PaperOnboardingFragment paperOnboardingFragment = PaperOnboardingFragment.newInstance(getDataforOnboarding());
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // fragmentTransaction method is used
+        // do all the transactions or changes
+        // between different fragments
+        fragmentTransaction.add(R.id.frame_layout, paperOnboardingFragment);
+
+        // all the changes are committed
+        fragmentTransaction.commit();
+
+    }
+
+    private ArrayList<PaperOnboardingPage> getDataforOnboarding() {
+
+        // the first string is to show the main title ,
+        // second is to show the message below the
+        // title, then color of background is passed ,
+        // then the image to show on the screen is passed
+        // and at last icon to navigate from one screen to other
+        PaperOnboardingPage source = new PaperOnboardingPage("Gfg", "Welcome to GeeksForGeeks", Color.parseColor("#ffb174"), R.drawable.message, R.drawable.smartphone);
+        PaperOnboardingPage source1 = new PaperOnboardingPage("Practice", "Practice questions from all topics", Color.parseColor("#22eaaa"), R.drawable.message, R.drawable.smartphone);
+        PaperOnboardingPage source2 = new PaperOnboardingPage("", " ", Color.parseColor("#ee5a5a"), R.drawable.message, R.drawable.smartphone);
+
+        // array list is used to store
+        // data of onbaording screen
+        ArrayList<PaperOnboardingPage> elements = new ArrayList<>();
+
+        // all the sources(data to show on screens)
+        // are added to array list
+        elements.add(source);
+        elements.add(source1);
+        elements.add(source2);
+        return elements;
     }
 }
