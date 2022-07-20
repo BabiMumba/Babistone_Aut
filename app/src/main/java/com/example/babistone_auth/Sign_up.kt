667c1aa519e3afb52240112fc0951ba56217d6b4
Babi Mumba <babimumba243@gmail.com>
2022-07-20 17:45:55 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/Sign_up.kt
+++ app/src/main/java/com/example/babistone_auth/Sign_up.kt
@@ -1,11 +1,29 @@
 package com.example.babistone_auth
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.babistone_auth.databinding.ActivityRegisteBinding
+import com.example.babistone_auth.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Sign_up : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var actionBar: ActionBar
+    //progress
+    private lateinit var progressDialog: ProgressDialog
+    //firebasse
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configuration de la boite de dialogue
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Patienter")
+        progressDialog.setMessage("creation du compte")
+        progressDialog.setCanceledOnTouchOutside(false)
     }
 }
