cf696c59b6c5ccf20bfaf3b846d9bf781121deab
Babi Mumba <babimumba243@gmail.com>
2022-07-20 16:38:00 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/Onboarding.kt
+++ app/src/main/java/com/example/babistone_auth/Onboarding.kt
@@ -22,6 +22,7 @@ class Onboarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
         setOnboardingItems()
         setupIndicators()
         setcurrentindicators(0)
