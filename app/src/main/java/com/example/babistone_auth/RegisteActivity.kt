bed5347585c8e196bbf2d054e5c2eb53cec7e59d
Babi Mumba <babimumba243@gmail.com>
2022-07-20 17:32:36 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/RegisteActivity.kt
+++ app/src/main/java/com/example/babistone_auth/RegisteActivity.kt
@@ -4,10 +4,14 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.LayoutInflater
 import androidx.appcompat.app.ActionBar
 import com.example.babistone_auth.databinding.ActivityRegisteBinding
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_registe.*
+import java.util.regex.Pattern
 
 class RegisteActivity : AppCompatActivity() {
     private lateinit var binding:ActivityRegisteBinding
@@ -16,8 +20,11 @@ class RegisteActivity : AppCompatActivity() {
     private lateinit var progressDialog:ProgressDialog
     //firebasse
     lateinit var firebaseAuth: FirebaseAuth
+    private  var mail = ""
+    private var paswrd = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityRegisteBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -30,6 +37,30 @@ class RegisteActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        binding.connectBtn.setOnClickListener {
+            validadta()
+
+
+        }
+    }
+
+    private fun validadta() {
+
+        mail = binding.mailBin.text.toString().trim()
+        paswrd = binding.passwordBin.text.toString().trim()
+        if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+            binding.mailBin.error = "mail invalide"
+        }else if (
+            TextUtils.isEmpty(paswrd)){
+            binding.passwordBin.error = "Entre un mot de passe"
+            }
+        else{
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+
     }
 
     private fun checkUser() {
