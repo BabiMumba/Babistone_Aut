a645432af0ac4f795722b75444e064bda90a5337
Babi Mumba <babimumba243@gmail.com>
2022-07-20 17:01:55 +0200

    add a new activity

---
+++ app/src/main/java/com/example/babistone_auth/RegisteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.babistone_auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registe)
+    }
+}
