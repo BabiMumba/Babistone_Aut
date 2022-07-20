278aebc1e198032b1d4360208e4057c7485f2a32
Babi Mumba <babimumba243@gmail.com>
2022-07-20 17:05:55 +0200

    add a new fiel

--- app/src/main/java/com/example/babistone_auth/RegisteActivity.kt
+++ app/src/main/java/com/example/babistone_auth/RegisteActivity.kt
@@ -2,10 +2,17 @@ package com.example.babistone_auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.ActionBar
+import com.example.babistone_auth.databinding.ActivityRegisteBinding
 
 class RegisteActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisteBinding
+    private lateinit var actionBar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisteBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_registe)
     }
 }
