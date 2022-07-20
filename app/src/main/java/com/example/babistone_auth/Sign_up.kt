96e635e4370bb3cd3fe9e67e255e869bd91e5dca
Babi Mumba <babimumba243@gmail.com>
2022-07-20 17:42:59 +0200

    add a new activity

---
+++ app/src/main/java/com/example/babistone_auth/Sign_up.kt
@@ -0,0 +1,11 @@
+package com.example.babistone_auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
