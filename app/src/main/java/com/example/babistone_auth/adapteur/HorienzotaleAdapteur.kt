8f96f25c976993d5ec444163607dd07b91e77fee
Babi Mumba <babimumba243@gmail.com>
2022-08-06 23:10:09 +0200

    add a new row list

--- app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
@@ -1,4 +1,28 @@
 package com.example.babistone_auth.adapteur
 
-class HorienzotaleAdapteur {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.babistone_auth.R
+
+class HorienzotaleAdapteur: RecyclerView.Adapter<HorienzotaleAdapteur.MyviewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
+      val view = LayoutInflater.from(parent.context).inflate(R.layout.row_item,parent,false)
+    }
+
+    override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    class MyviewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    }
+
+
 }
