943d56ca5227fedd5eea4505e80e2d5d255ef083
Babi Mumba <babimumba243@gmail.com>
2022-08-07 01:45:08 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/Horinzatale_activity.kt
+++ app/src/main/java/com/example/babistone_auth/Horinzatale_activity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.babistone_auth.R
+import com.example.babistone_auth.adapteur.Category2
 import com.example.babistone_auth.adapteur.HorienzotaleAdapteur
 import com.example.babistone_auth.model.fashio_model
 import kotlinx.android.synthetic.main.activity_horinzatale.*
@@ -12,7 +13,9 @@ import kotlinx.android.synthetic.main.activity_horinzatale.*
 class Horinzatale_activity : AppCompatActivity() {
 
     val liste_fashion = ArrayList<fashio_model>()
+    val liste_fashion2 = ArrayList<fashio_model>()
     val display = ArrayList<fashio_model>()
+    val display2 = ArrayList<fashio_model>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +24,22 @@ class Horinzatale_activity : AppCompatActivity() {
 
         liste_fashion.add(fashio_model("chemise",300,R.drawable.chemise1))
         liste_fashion.add(fashio_model("chemise 2",100,R.drawable.chemise2))
-        liste_fashion.add(fashio_model("banane",100,R.drawable.banane))
+        liste_fashion.add(fashio_model("banane",100,R.drawable.air_pot))
         liste_fashion.add(fashio_model("casque",100,R.drawable.casque))
-        liste_fashion.add(fashio_model("beignet",100,R.drawable.beignet))
-        liste_fashion.add(fashio_model("marienette",100,R.drawable.marie))
-        liste_fashion.add(fashio_model("Humbergeur",100,R.drawable.emburger))
+        liste_fashion2.add(fashio_model("beignet",100,R.drawable.beignet))
+        liste_fashion2.add(fashio_model("marienette",100,R.drawable.marie))
+        liste_fashion2.add(fashio_model("Humbergeur",100,R.drawable.emburger))
+
         display.addAll(liste_fashion)
+        display2.addAll(liste_fashion2)
+
+
         val adapteur = HorienzotaleAdapteur(this,display)
+        val adapteur2 = Category2(this,display2)
         Horiz_Recy.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        category2.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         Horiz_Recy.adapter = adapteur
+        category2.adapter = adapteur2
 
 
     }
