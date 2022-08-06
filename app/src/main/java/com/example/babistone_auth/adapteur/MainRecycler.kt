3c14d1987d55f2a8362ca567ae252ef6803eec25
Babi Mumba <babimumba243@gmail.com>
2022-08-06 12:50:57 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/adapteur/MainRecycler.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/MainRecycler.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.babistone_auth.R
 import com.example.babistone_auth.model.allcategories
@@ -11,15 +12,22 @@ import com.example.babistone_auth.model.allcategories
 class MainRecycler(private val context: Context, private val allcategories: List<allcategories>):
     RecyclerView.Adapter<MainRecycler.MainViewHolder>() {
     class MainViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+       lateinit var category_title: TextView
+       init {
+           category_title = itemView.findViewById(R.id.title_text)
+       }
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+
         return MainViewHolder(LayoutInflater.from(context).inflate(R.layout.main_recycler_row_item,parent,false))
 
     }
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+
 
     }
 
