e55e7a4deb4ec9c91ebb57871c37ad2e4e50db8c
Babi Mumba <babimumba243@gmail.com>
2022-08-06 13:11:09 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/MainRecyclerv.java
+++ app/src/main/java/com/example/babistone_auth/MainRecyclerv.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RecysclerView_nested extends AppCompatActivity {
+public class MainRecyclerv extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recyscler_view_nested);
+        setContentView(R.layout.main_recycler);
     }
 }
