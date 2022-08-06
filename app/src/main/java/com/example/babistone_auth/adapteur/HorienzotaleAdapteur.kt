2a84cb350291a71a417f83e5a71025d7b3855a07
Babi Mumba <babimumba243@gmail.com>
2022-08-06 23:50:33 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
@@ -1,12 +1,15 @@
 package com.example.babistone_auth.adapteur
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.babistone_auth.R
+import com.example.babistone_auth.model.fashio_model
 
-class HorienzotaleAdapteur: RecyclerView.Adapter<HorienzotaleAdapteur.MyviewHolder>() {
+class HorienzotaleAdapteur(private var context: Context,private var fashioModel: ArrayList<fashio_model>):
+    RecyclerView.Adapter<HorienzotaleAdapteur.MyviewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
       val view = LayoutInflater.from(parent.context).inflate(R.layout.row_item,parent,false)
