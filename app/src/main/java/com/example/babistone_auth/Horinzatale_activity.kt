d1c06491c4db3a09bd05795cbb96359b21bbbad4
Babi Mumba <babimumba243@gmail.com>
2022-08-07 01:00:17 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/Horinzatale_activity.kt
+++ app/src/main/java/com/example/babistone_auth/Horinzatale_activity.kt
@@ -7,30 +7,28 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.babistone_auth.R
 import com.example.babistone_auth.adapteur.HorienzotaleAdapteur
 import com.example.babistone_auth.model.fashio_model
+import kotlinx.android.synthetic.main.activity_horinzatale.*
 
 class Horinzatale_activity : AppCompatActivity() {
-    private lateinit var recyclerview : RecyclerView
-    private lateinit var adapteur : HorienzotaleAdapteur
-    val liste_fashion = ArrayList<fashio_model>()
 
+    val liste_fashion = ArrayList<fashio_model>()
+    val display = ArrayList<fashio_model>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_horinzatale)
-        recyclerview = findViewById(R.id.Horiz_Recy)
+
 
         liste_fashion.add(fashio_model("chemise",100,R.drawable.casque))
         liste_fashion.add(fashio_model("chemisier",100,R.drawable.casque))
         liste_fashion.add(fashio_model("chemise 3",100,R.drawable.casque))
         liste_fashion.add(fashio_model("chemise 4",100,R.drawable.casque))
+        display.addAll(liste_fashion)
+        val adapteur = HorienzotaleAdapteur(this,display)
+        Horiz_Recy.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        Horiz_Recy.adapter = adapteur
+
 
     }
-    private fun setMainfashion(fashion:ArrayList<fashio_model>){
-        recyclerview = findViewById(R.id.Horiz_Recy)
-        val lm: RecyclerView.LayoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        recyclerview!!.layoutManager
-        adapteur = HorienzotaleAdapteur(this,fashion)
-        recyclerview!!.adapter = adapteur
-    }
+
 }
--- app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/HorienzotaleAdapteur.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.babistone_auth.R
 import com.example.babistone_auth.model.fashio_model
@@ -12,27 +14,35 @@ import kotlinx.android.synthetic.main.row_item.view.*
 class HorienzotaleAdapteur( var context: Context, var fashioModel: ArrayList<fashio_model>):
     RecyclerView.Adapter<HorienzotaleAdapteur.MyviewHolder>() {
 
+    class MyviewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+         var nom:TextView
+         var prix:TextView
+         var image:ImageView
+        init {
+            nom = itemView.findViewById(R.id.nom_article)
+            prix = itemView.findViewById(R.id.prix)
+            image = itemView.findViewById(R.id.image_article)
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
       val view = LayoutInflater.from(parent.context).inflate(R.layout.row_item,parent,false)
         return  MyviewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+        val fashion = fashioModel[position]
+        holder.prix.text = fashion.prix.toString()
+        holder.nom.text = fashion.nom.toString()
+        holder.image.setImageResource(fashion.image)
+
 
     }
     override fun getItemCount(): Int {
         return fashioModel.size
     }
-    class MyviewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        fun binItem(fashioModel: fashio_model){
-            itemView.nom_article.text = fashioModel.nom
-            itemView.prix.text = fashioModel.prix.toString()
-            itemView.image_article.setImageResource(fashioModel.image)
 
-
-        }
-
-    }
 
 
 }
