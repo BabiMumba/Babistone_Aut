4267bc12ad41535a80ce78939ca85e5bcb18b6df
Babi Mumba <babimumba243@gmail.com>
2022-08-07 02:27:48 +0200

    add a new file

--- app/src/main/java/com/example/babistone_auth/DetailleActivity.kt
+++ app/src/main/java/com/example/babistone_auth/DetailleActivity.kt
@@ -1,12 +1,18 @@
 package com.example.babistone_auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.babistone_auth.R
+import kotlinx.android.synthetic.main.activity_detaille.*
 
 class DetailleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detaille)
+        var intent = intent
+        val ttr = intent.getIntExtra("image",R.drawable.pec)
+        img_detaille.setImageResource(ttr)
+
     }
 }
