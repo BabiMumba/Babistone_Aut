265ce1bfeb43865e6796c5a20202f1750e8a6a2c
Babi Mumba <babimumba243@gmail.com>
2022-08-21 23:45:53 +0200

    add new file

--- app/src/main/java/com/example/babistone_auth/adapteur/DataAdapter.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/DataAdapter.kt
@@ -11,20 +11,28 @@ import com.example.babistone_auth.model.library
 class DataAdapter(private val liste_book:ArrayList<library>):RecyclerView.Adapter<DataAdapter.MyviewHolde>() {
 
     public class MyviewHolde(itemView: View):RecyclerView.ViewHolder(itemView) {
-        val name_book:TextView = itemView.findViewById(R.id.name_bk)
-        val name_autor:TextView = itemView.findViewById(R.id.name_aut)
+        val name_book:TextView
+        val name_autor:TextView
+        init {
+            name_book = itemView.findViewById(R.id.name_bk)
+            name_autor= itemView.findViewById(R.id.name_aut)
+        }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataAdapter.MyviewHolde {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolde {
         val itemview = LayoutInflater.from(parent.context).inflate(R.layout.row_item_data,parent,false)
         return MyviewHolde(itemview)
     }
 
-    override fun onBindViewHolder(holder: DataAdapter.MyviewHolde, position: Int) {
-        val library:library = liste_book[position]
-        holder.name_book.text = library.name_book.toString()
-        holder.name_autor.text = library.name_autor.toString()
+    override fun onBindViewHolder(holder: MyviewHolde, position: Int) {
+        val lbrary:library = liste_book[position]
+
+        holder.name_book.text = lbrary.name_book.toString()
+        holder.name_autor.text = lbrary.name_autor.toString()
+        holder.itemView.setOnClickListener {
+
+        }
     }
 
     override fun getItemCount(): Int {
