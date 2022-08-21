3d5897b05eaf8dd8e78dda9a380909c8f0d55a69
Babi Mumba <babimumba243@gmail.com>
2022-08-21 23:07:58 +0200

    add ewn file

--- app/src/main/java/com/example/babistone_auth/adapteur/DataAdapter.kt
+++ app/src/main/java/com/example/babistone_auth/adapteur/DataAdapter.kt
@@ -1,24 +1,32 @@
 package com.example.babistone_auth.adapteur
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.babistone_auth.R
 import com.example.babistone_auth.model.library
 
 class DataAdapter(private val liste_book:ArrayList<library>):RecyclerView.Adapter<DataAdapter.MyviewHolde>() {
     class MyviewHolde(itemView: View):RecyclerView.ViewHolder(itemView) {
+        val name_book:TextView = itemView.findViewById(R.id.name_bk)
+        val name_autor:TextView = itemView.findViewById(R.id.name_aut)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataAdapter.MyviewHolde {
-        TODO("Not yet implemented")
+        val itemview = LayoutInflater.from(parent.context).inflate(R.layout.row_item_data,parent,false)
+        return MyviewHolde(itemview)
     }
 
     override fun onBindViewHolder(holder: DataAdapter.MyviewHolde, position: Int) {
-        TODO("Not yet implemented")
+        val library:library = liste_book[position]
+        holder.name_book.text = library.name_book
+        holder.name_autor.text = library.name_autor
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        liste_book.size
     }
 }
