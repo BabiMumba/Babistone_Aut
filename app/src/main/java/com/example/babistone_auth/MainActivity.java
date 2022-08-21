9727db9ed5f9a57c4930c30d42f33a14d8fb38bb
Babi Mumba <babimumba243@gmail.com>
2022-08-21 19:40:20 +0200

    add new file

--- app/src/main/java/com/example/babistone_auth/MainActivity.java
+++ app/src/main/java/com/example/babistone_auth/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.babistone_auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,5 +48,56 @@ public class MainActivity extends AppCompatActivity {
         courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
         submitCourseBtn = findViewById(R.id.idBtnSubmitCourse);
 
+        // adding on click listener for button
+        submitCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // getting data from edittext fields.
+                courseName = courseNameEdt.getText().toString();
+                courseDescription = courseDescriptionEdt.getText().toString();
+                courseDuration = courseDurationEdt.getText().toString();
+
+                // validating the text fields if empty or not.
+                if (TextUtils.isEmpty(courseName)) {
+                    courseNameEdt.setError("Please enter Course Name");
+                } else if (TextUtils.isEmpty(courseDescription)) {
+                    courseDescriptionEdt.setError("Please enter Course Description");
+                } else if (TextUtils.isEmpty(courseDuration)) {
+                    courseDurationEdt.setError("Please enter Course Duration");
+                } else {
+                    // calling method to add data to Firebase Firestore.
+                    addDataToFirestore(courseName, courseDescription, courseDuration);
+                }
+            }
+        });
+
+    }
+
+    private void addDataToFirestore(String courseName, String courseDescription, String courseDuration) {
+
+        // creating a collection reference
+        // for our Firebase Firetore database.
+        CollectionReference dbCourses = db.collection("Courses");
+
+        // adding our data to our courses object class.
+        Courses courses = new Courses(courseName, courseDescription, courseDuration);
+
+        // below method is use to add data to Firebase Firestore.
+        dbCourses.add(courses).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                // after the data addition is successful
+                // we are displaying a success toast message.
+                Toast.makeText(MainActivity.this, "Your Course has been added to Firebase Firestore", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // this method is called when the data addition process is failed.
+                // displaying a toast message when data addition is failed.
+                Toast.makeText(MainActivity.this, "Fail to add course \n" + e, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
