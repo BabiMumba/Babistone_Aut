d6064975d33e2625bb78308d2b65d43c1bd7e31b
Babi Mumba <babimumba243@gmail.com>
2022-08-21 19:51:08 +0200

    add new file

--- app/src/main/java/com/example/babistone_auth/FirestorePage.kt
+++ app/src/main/java/com/example/babistone_auth/FirestorePage.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FirestorePage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firestore_page)
+
     }
 }
