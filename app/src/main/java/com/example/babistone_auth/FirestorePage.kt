041414449a8925df1d3ed786468313afead545bd
Babi Mumba <babimumba243@gmail.com>
2022-08-21 20:43:14 +0200

    add new file

--- app/src/main/java/com/example/babistone_auth/FirestorePage.kt
+++ app/src/main/java/com/example/babistone_auth/FirestorePage.kt
@@ -5,15 +5,12 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class FirestorePage : AppCompatActivity() {
-
-
-
-    private var db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +20,27 @@ class FirestorePage : AppCompatActivity() {
 
 
         add_value.setOnClickListener {
-            db.collection("info_book")
-                .add(add_book(name_book.text.toString(),autor.text.toString()))
-                .addOnSuccessListener {
-
-                    Toast.makeText(this, "livre ajouter", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "livre non ajouter", Toast.LENGTH_SHORT).show()
-                }
+            val namebook = name_book.text.toString()
+            val autor = autor.text.toString()
+           add_book(namebook,autor)
         }
 
 // Add a new document with a generated ID
     }
     private fun add_book(name_book:String, autor_name:String ){
+        val db = FirebaseFirestore.getInstance()
+        val user:MutableMap<String, Any> = HashMap()
+        user["name book"] = name_book
+        user["name autor"] = autor_name
 
+        db.collection("info_book")
+            .add(user)
+            .addOnSuccessListener {
+                Toast.makeText(this, "livre ajouter", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "livre non ajouter", Toast.LENGTH_SHORT).show()
+            }
 
     }
 }
