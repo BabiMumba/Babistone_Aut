e8dca40dde929f454366b568a453fd166090d4da
Babi Mumba <babimumba243@gmail.com>
2022-08-21 19:38:09 +0200

    assd new file

--- app/src/main/java/com/example/babistone_auth/MainActivity.java
+++ app/src/main/java/com/example/babistone_auth/MainActivity.java
@@ -3,8 +3,27 @@ package com.example.babistone_auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // creating variables for our edit text
+    private EditText courseNameEdt, courseDurationEdt, courseDescriptionEdt;
+
+    // creating variable for button
+    private Button submitCourseBtn;
+
+    // creating a strings for storing
+    // our values from edittext fields.
+    private String courseName, courseDuration, courseDescription;
+
+    // creating a variable
+    // for firebasefirestore.
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
