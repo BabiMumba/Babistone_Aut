07f90bccb3a21b41ba8d5a804c6abb3ed7269d3c
Babi Mumba <babimumba243@gmail.com>
2022-08-22 22:22:28 +0200

    add new file

---
+++ app/src/main/java/com/example/babistone_auth/ShowBookFirestore.kt
@@ -0,0 +1,11 @@
+package com.example.babistone_auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowBookFirestore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_book_firestore)
+    }
+}
